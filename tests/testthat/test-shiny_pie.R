83641c891e4af5ee0c726352d896f67fbfd89fbb
Jai Desai <44884343+jairav@users.noreply.github.com>
2023-04-05 13:24:10 -0400

    shiny pie test added. uses vdiffr.

---
+++ tests/testthat/test-shiny_pie.R
@@ -0,0 +1,42 @@
+# Load SingleCellExperiment object from HDF5 storage
+sce <- AML_SCE()
+
+# shiny_pie test: test if plot runs without error
+test_that("Shiny_pie runs without error", {
+  # Plotting with SingleCellExperiment object
+  expect_no_error(
+    SCEPlots::shiny_pie(
+      sce,
+      patient_colname = "Batch",
+      group_by = "seurat_clusters"
+    )
+  )
+  # Seurat object
+  expect_no_error(
+    SCEPlots::shiny_pie(
+      AML_Seurat,
+      patient_colname = "Batch",
+      group_by = "seurat_clusters"
+    )
+  )
+})
+
+test_that("Shiny_pie creates doppelganger.", {
+  vdiffr::expect_doppelganger(
+    title = "shiny_pie.SingleCellExperiment test", 
+    SCEPlots::shiny_pie(
+      sce,
+      patient_colname = "Batch",
+      group_by = "seurat_clusters"
+    )
+  )
+  #Seurat Object
+  vdiffr::expect_doppelganger(
+    title = "shiny_pie.Seurat test", 
+    SCEPlots::shiny_pie(
+      AML_Seurat,
+      patient_colname = "Batch",
+      group_by = "seurat_clusters"
+    )
+  )
+})
