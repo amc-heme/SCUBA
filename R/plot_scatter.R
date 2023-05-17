9072ae48ed4be26c693a43e65238510549982a5d
Bill Showers <william.showers@ucdenver.edu>
2023-05-16 19:36:31 -0600

    Feature scatterplot function
    Created plot_scatter based on Seurat::FeatureScatter (#18).

--- R/plot_scatter.R
+++ R/plot_scatter.R
@@ -4,16 +4,18 @@
 #' set of single cells. Cells are colored by their identity class. Pearson
 #' correlation between the two features is displayed above the plot.
 #'
-#' @param object Seurat object
-#' @param feature_1 First feature to plot. Typically feature expression but can also
-#' be metrics, PC scores, etc. - anything that can be retreived with FetchData
+#' @param object a Seurat object or a SingleCellExperiment object
+#' @param group_by Name of one or more metadata variables to group
+#' (color) cells by. Unlike \code{Seurat::FeatureScatter}, at least
+#' one group_by variable must be defined
+#' @param feature_1 First feature to plot. Typically feature
+#' expression but can also be QC metrics, PC scores, etc. - anything
+#' that can be retrieved with FetchData
 #' @param feature_2 Second feature to plot.
 #' @param cells Cells to include on the scatter plot.
 #' @param shuffle Whether to randomly shuffle the order of points. This can be
 #' useful for crowded plots if points of interest are being buried. (default is FALSE)
 #' @param seed Sets the seed if randomly shuffling the order of points.
-#' @param group_by Name of one or more metadata columns to group (color) cells by
-#' (for example, orig.ident); pass 'ident' to group by identity class
 #' @param cols Colors to use for identity class plotting.
 #' @param pt_size Size of the points on the plot
 #' @param shape_by Ignored for now
@@ -35,63 +37,87 @@
 #' @importFrom patchwork wrap_plots
 #'
 #' @export
-#' @concept visualization
 #'
-#' @aliases GenePlot
-#'
-FeatureScatter <-
+plot_scatter <-
   function(
     object,
     feature_1,
     feature_2,
+    group_by,
     cells = NULL,
     shuffle = FALSE,
     seed = 1,
-    group_by = NULL,
     cols = NULL,
     pt_size = 1,
     shape_by = NULL,
     span = NULL,
     smooth = FALSE,
     combine = TRUE,
-    slot = 'data',
+    slot = NULL,
     plot_cor = TRUE,
     raster = NULL,
     raster_dpi = c(512, 512),
     jitter = FALSE
   ){
-    # 1. Define cells to use: if unspecified, plot all cells
+    # 1. Process parameters and define defaults
+    ## 1.1. cells to use: if unspecified, plot all cells
     cells <- cells %||% get_all_cells(object)
 
-    ## 1.2. Process shuffle settings
+    ## 1.2. Slot: use default slot if undefined
+    slot <- slot %||% default_slot(object)
+
+    ## 1.3. Process shuffle settings
     if (isTRUE(x = shuffle)) {
       set.seed(seed = seed)
       cells <- sample(x = cells)
     }
 
-    object[['ident']] <- Idents(object = object)
-    group_by <- group_by %||% 'ident'
+    # 2. Fetch data
+    ## 2.1. Group by data
+    groups <-
+      fetch_metadata(
+        object,
+        # There can be multiple group_by variables
+        vars = group_by,
+        cells = cells
+        )
 
-    data <-  FetchData(
-      object = object,
-      vars = c(feature_1, feature_2, group_by),
-      cells = cells,
-      slot = slot
-    )
+    ## 2.2. Feature expression data
+    data <-
+      FetchData(
+        object,
+        vars = c(feature_1, feature_2),
+        cells = cells,
+        slot = slot
+      )
+
+    ## 2.3. Return error if either feature is not found
     if (!grepl(pattern = feature_1, x = colnames(x = data)[1])) {
       stop("Feature 1 (", feature_1, ") not found.", call. = FALSE)
     }
     if (!grepl(pattern = feature_2, x = colnames(x = data)[2])) {
       stop("Feature 2 (", feature_2, ") not found.", call. = FALSE)
     }
-    data <- as.data.frame(x = data)
-    feature_1 <-  colnames(x = data)[1]
-    feature_2 <-  colnames(x = data)[2]
+
+    ## 2.4. Join group_by data to expression data
+    data <-
+      cbind(
+        data,
+        groups
+        )
+
+    ## 2.5. Explicitly set column names to features
+    feature_1 <- colnames(x = data)[1]
+    feature_2 <- colnames(x = data)[2]
+
+    ## 2.6. Convert group_by column(s) to factors if not already
     for (group in group_by) {
       if (!is.factor(x = data[, group])) {
         data[, group] <- factor(x = data[, group])
       }
     }
+
+    # 3. Make plots
     plots <- lapply(
       X = group_by,
       FUN = function(x) {
