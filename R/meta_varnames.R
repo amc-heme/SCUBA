1cd8767608e1fddd677e2ccf231cba9209e28508
Bill Showers <william.showers@ucdenver.edu>
2023-05-24 16:34:42 -0600

    S3 methods: show all metadata variables
    Created a set of S3 methods to show the names of all metadata variables in an object (#49). This is based on a common operation in scExploreR, and allows this operation to be performed on either Seurat or SingleCellExperiment objects (amc-heme/scExploreR#221).

---
+++ R/meta_varnames.R
@@ -0,0 +1,52 @@
+#' Object metadata summary
+#'
+#' Returns the names of all metadata variables in an object.
+#'
+#' @param object a single-cell object. Currently, Seurat and
+#' SingleCellExperiment objects are supported.
+#'
+#' @rdname meta_varnames
+#'
+#' @export
+meta_varnames <-
+  function(
+    object
+  ){
+    UseMethod("meta_varnames")
+  }
+
+#' Function to display an error message when an unsupported object
+#' type is detected
+#'
+#' @noRd
+#' @export
+meta_varnames.default <-
+  function(
+    object
+  ){
+    warning(
+      paste0(
+        "meta_varnames does not know how to handle object of class ",
+        paste(class(object), collapse = ", "),
+        ". Currently supported classes: Seurat and SingleCellExperiment."
+      )
+    )
+  }
+
+#' @describeIn meta_varnames Seurat objects
+#' @export
+meta_varnames.Seurat <-
+  function(
+    object
+  ){
+    colnames(object@meta.data)
+  }
+
+#' @describeIn meta_varnames SingleCellExperiment objects
+#' @export
+meta_varnames.SingleCellExperiment <-
+  function(
+    object
+  ){
+    colnames(colData(object))
+  }
