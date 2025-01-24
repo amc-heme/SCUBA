78d0bf03d405f65e98f71d3c722a468a3c74b0d9
Bill Showers <45372167+wish1832@users.noreply.github.com>
2025-01-24 13:07:47 -0700

    Add features_in_assay method back
    The features_in_assay method was created in the SCUBA package but was moved to scExploreR for an unknown reason. This is a useful utility method for fetching feature expression data, and will be used in the user guide vignette, so it was added back to SCUBA.

---
+++ R/features_in_assay.R
@@ -0,0 +1,91 @@
+#' Get names of all features in an assay/experiment
+#'
+#' Returns the names of all features in an assay/modality
+#'
+#' @param object a single-cell object. 
+#' @param assay the name of an assay/modality for which to view features
+#'
+#' @rdname features_in_assay
+#' 
+features_in_assay <-
+  function(
+    object,
+    assay
+  ){
+    if (length(assay) > 1){
+      stop("Only one assay/experiment name may be passed to `assay`.")
+    }
+    
+    UseMethod("features_in_assay")
+  }
+
+#' Function to display an error message when an unsupported object
+#' type is detected
+#'
+#' @noRd
+#' @export
+features_in_assay.default <-
+  function(
+    object
+  ){
+    warning(
+      paste0(
+        "features_in_assay does not know how to handle object of class ",
+        paste(class(object), collapse = ", "),
+        ". Currently supported classes: Seurat and SingleCellExperiment."
+      )
+    )
+  }
+
+#' @describeIn features_in_assay Seurat objects
+#' @export
+features_in_assay.Seurat <-
+  function(
+    object,
+    assay
+  ){
+    if (!assay %in% assay_names(object)){
+      stop("Assay", assay, "not found in the current object.")
+    }
+    
+    # Return rownames of assay entered
+    rownames(object@assays[[assay]])
+  }
+
+#' @describeIn features_in_assay SingleCellExperiment objects
+#' @export
+features_in_assay.SingleCellExperiment <-
+  function(
+    object,
+    assay
+  ){
+    if (!assay %in% assay_names(object)){
+      stop("Assay", assay, "not found in the current object.")
+    }
+    
+    if (assay == mainExpName(object)){
+      rownames(object)
+    } else {
+      rownames(altExps(object)[[assay]])
+    }
+  }
+
+#' @describeIn features_in_assay Anndata objects
+#' @export
+features_in_assay.AnnDataR6 <-
+  function(
+    object,
+    assay
+  ){
+    if (!assay %in% assay_names(object)){
+      stop("Assay", assay, "not found in the current object.")
+    }
+    
+    if (assay == "X"){
+      # For the X matrix, feature names are in var_names
+      object$var_names
+    } else {
+      # Otherwise, use the column names of the matrix stored in obsm
+      colnames(object$obsm[[assay]])
+    }
+  }
