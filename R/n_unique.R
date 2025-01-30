3c77e2ac07b8f96ec1cbf3e282f9758647c5d1de
kriemo <kent.riemondy@refinedscience.com>
2025-01-30 08:15:58 -0700

    fix rd xrefs and URLs using markdown instead of rd syntax

--- R/n_unique.R
+++ R/n_unique.R
@@ -1,8 +1,7 @@
 #' Object metadata: number of unique values
 #'
 #' Returns the number of unique values in a single cell object, for the specified
-#' metadata variable. Any object type supported by
-#' \code{\link[unique_values]{unique_values}} may be used.
+#' metadata variable. Any object type supported by [unique_values] may be used.
 #'
 #' @param object A single cell object.
 #' @param meta_var Name of a metadata variable.
