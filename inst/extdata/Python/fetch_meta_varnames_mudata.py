13a4483879700fcc84feba70c3822aac9c5c49ef
Bill Showers <45372167+wish1832@users.noreply.github.com>
2025-07-18 15:24:14 -0600

    meta_varnames method

--- inst/extdata/Python/fetch_meta_varnames_mudata.py
+++ inst/extdata/Python/fetch_meta_varnames_mudata.py
@@ -4,12 +4,12 @@ import pandas as pd
 import numpy as np
 
 
-def fetch_meta_varnames_targeted(obj, use_mudata_obsm, mod):
+def fetch_meta_varnames_targeted(obj, use_mudata_obs, mod):
     """
     fetch_meta_varnames_targeted
     """
-    if use_mudata_obsm == True:
-        print("Pulling from main obsm slot of mudata object")
+    if use_mudata_obs == True:
+        print("Pulling from main obs slot of mudata object")
     
         keys = obj.obs_keys()
         
@@ -18,17 +18,17 @@ def fetch_meta_varnames_targeted(obj, use_mudata_obsm, mod):
     else:
         if mod == None:
             raise ValueError(
-                "mod must be defined if use_mudata_obsm is False."
+                "mod must be defined if use_mudata_obs is False."
                 )
     
-        print("Pulling from obsm slot of", mod, sep = " ")
+        print("Pulling from obs slot of", mod, sep = " ")
         
         keys = obj[mod].obs_keys()
         
         return keys
 
 
-def fetch_meta_varnames_mudata(obj, modal):
+def fetch_meta_varnames_mudata(obj, mod):
     """
     fetch_meta_varnames_mudata
     
@@ -36,34 +36,37 @@ def fetch_meta_varnames_mudata(obj, modal):
     """
     # Directs fetch_meta_varnames to pull from the obsm slot of the main mudata object
     # if True
-    use_mudata_obsm = False
+    use_mudata_obs = False
 
     # Search for modality in the mudata object first
     # If not found, set to use the main object obsm slot
-    if modal in obj.mod_names:
-        use_mudata_obsm = False
+    if mod in obj.mod_names:
+        use_mudata_obs = False
     elif modal == None:
-        # Set use_mudata_obsm to True to pull from the main object obsm slot
-        use_mudata_obsm = True
+        # Set use_mudata_obs to True to pull from the main object obsm slot
+        use_mudata_obs = True
     else:
-        # Set use_mudata_obsm to True if modality is not found in the object
-        print(modal, " modality not found in mudata object. Pulling metadata variable names from main mudata object obsm slot")
-        use_mudata_obsm = True
+        # Set use_mudata_obs to True if modality is not found in the object
+        warning(
+            paste0(
+                mod, " modality not found in mudata object. Pulling ",
+                "metadata variable names from main mudata object obs slot."
+                )
+            )
+        use_mudata_obs = True
         
     # Fetch reduction from the location determined above
-    if use_mudata_obsm == True:
+    if use_mudata_obs == True:
         keys = fetch_meta_varnames_targeted(
             obj, 
-            use_mudata_obsm = True, 
+            use_mudata_obs = True, 
             mod = None
             )
-    
-        return keys
     else:
         keys = fetch_meta_varnames_targeted(
             obj, 
-            use_mudata_obsm = False, 
-            mod = modal
+            use_mudata_obs = False, 
+            mod = mod
             )
     
-        return keys
+    return keys
